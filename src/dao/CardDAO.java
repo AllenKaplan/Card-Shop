b0286aff58e602aaf2fb112f98d2dd654ff83373
JeremyWinkler <jeremy.winkler1@gmail.com>
2020-03-09 13:55:15 -0400

    work on home page

--- src/dao/CardDAO.java
+++ src/dao/CardDAO.java
@@ -1,5 +1,23 @@
 package dao;
 
-public class CardDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class CardDAO {
+	private DataSource dataSource;
+
+	public CardDAO() throws ClassNotFoundException {
+		try {
+			dataSource = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
 }
