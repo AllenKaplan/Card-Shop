b7a3611f3b59852dcb9870a3e4e7ecc29d008f0e
DamanB <damanbharaj@gmail.com>
2020-03-11 15:35:04 -0400

    Added a Derby database and created/filled Cards table

--- database/Cards.sql
+++ database/Cards.sql
@@ -1,20 +1,19 @@
-drop table if exists Cards;
+DROP TABLE IF EXISTS Cards;
 
-create table Cards (
+CREATE TABLE Cards (
     number int not null,
-    name varchar(100) not null,
+    name varchar(200) not null,
     -- For card 101, Rank/Limit D-70, this field is D
     rank varchar(2) not null,
     -- For card 101, Rank/Limit D-70, this field is 70
-    limit
-        int not null,
-        description varchar(200) not null,
-        -- Specified Slot / Unspecified Slot / Spell
-        class varchar(15) not null,
-        -- 2 letter short form: ex (SR) => Short Range
-        spellType varchar(10),
-        -- Cards each have a number thats unique to that card
-        primary key (number)
+    limit int not null,
+    description varchar(500) not null,
+    -- Specified Slot / Unspecified Slot / Spell
+    class varchar(20) not null,
+    -- 2 letter short form: ex (SR) => Short Range
+    spellType varchar(20),
+    -- Cards each have a number thats unique to that card
+    primary key (number)
 );
 
 -- Specified Slot Cards
@@ -23,8 +22,10 @@ insert into
         number,
         name,
         rank,
-        limit
-, description, class, spellType
+        limit,
+        description, 
+        class, 
+        spellType
     )
 values
 (0,'Rulers Blessing (Blessing of Ruler)','SS',1,'A castle given as a prize for winning the contest, town with population 10,000 included. Its residents will live according to whatever laws and commands you issue.','Specified Slot',null),
@@ -135,8 +136,7 @@ insert into
         number,
         name,
         rank,
-        limit
-, description, class, spellType
+        limit, description, class, spellType
     )
 values
 (100,'Map of the Island empty','G',400,'A map of the island. No towns or markers are currently shown. The user will automatically fill these in on their travels.','Unspecified Slot',null),
