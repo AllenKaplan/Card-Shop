21ff45b2d7c90aa08be301a64c29624b235df8c6
AllenKaplan <allenkaplan07@gmail.com>
2020-04-02 15:46:24 -0400

    img fix

--- src/bean/ProductBean.java
+++ src/bean/ProductBean.java
@@ -1,7 +1,5 @@
 package bean;
 
-import java.util.List;
-
 public abstract class ProductBean {
 	private String name;
 	private String description;
@@ -41,11 +39,11 @@ public abstract class ProductBean {
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	public String getImages() {
+	public String getImgs() {
 		return img;
 	}
-	public void setImages(String images) {
-		this.img = images;
+	public void setImgs(String img) {
+		this.img = img;
 	}
 	
 }
