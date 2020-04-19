3f7fdffd8b07e2551c057ea0dc27216d9010ad99
JeremyWinkler <jeremy.winkler1@gmail.com>
2020-04-19 11:39:09 -0400

    work on login. registration page made

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -4,19 +4,39 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import bean.UserBean;
+
 public class UserDAO {
-	private DataSource ds;
+	private DataSource dataSource;
 
 	public UserDAO() throws ClassNotFoundException {
-//		try {
-//			ds = (DataSource) (new InitialContext()).lookup("?"); // ? was java:/comp/env/jdbc/EECS but this needs to be changed
-//		} catch (NamingException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			dataSource = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public boolean login(String username, String password) {
-		return (username.equals("Jer") && password.equals("notKevin"));
+	/**
+	 * Tries to login
+	 * @param username
+	 * @param password
+	 * @return The type (customer, admin, or partner) of the account that logged in. If no account found, returns null
+	 */
+	public String login(String username, String password) {
+		return (username.equals("user") && password.equals("password")) ? "customer" : null;
+	}
+	
+	/**
+	 * Registers a user with the customer account type
+	 * @param username
+	 * @param password
+	 * @param newUser - the user to be registered
+	 * 
+	 * @throws throws exception if failed to register
+	 */
+	public void register(String username, String password, UserBean newUser) throws Exception {
+		
 	}
 }
